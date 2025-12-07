e3ce67428792ac2bdb13adb8c30a37e60cbf76f8
roshanjoshi2000 <meroshangenius@gmail.com>
2025-12-07 23:56:19 +0000

    Add files via upload

---
+++ search game 2.php
@@ -0,0 +1,66 @@
+<?php
+include("db.php");
+
+// If AJAX request, return ONLY the table rows
+if(isset($_POST['ajax']) && $_POST['ajax'] == 1){
+
+    $keywords = $_POST['keywords'] ?? '';
+
+    $sql = "SELECT * FROM videogames
+            WHERE game_name LIKE '%{$keywords}%'
+            ORDER BY released_date";
+
+    $results = mysqli_query($mysqli, $sql);
+
+    echo "<table>";
+
+    while($row = mysqli_fetch_assoc($results)) {
+        echo "<tr>";
+        echo "<td><a href='game-details.php?id={$row['game_id']}'>".$row['game_name']."</a></td>";
+        echo "<td>".$row['rating']."</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+    exit; // Stop the page here for AJAX
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>AJAX Game Search</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+
+<body>
+
+<h1>Search Results</h1>
+
+<input type="text" id="keywords" placeholder="Search games...">
+<div id="results">Loading...</div>
+
+<script>
+$(document).ready(function(){
+
+    // Load all games initially
+    loadGames("");
+
+    // Function to load results using AJAX
+    function loadGames(query){
+        $.post("search-games.php", { keywords: query, ajax: 1 }, function(data){
+            $("#results").html(data);
+        });
+    }
+
+    // Search as the user types
+    $("#keywords").keyup(function(){
+        let query = $(this).val();
+        loadGames(query);
+    });
+
+});
+</script>
+
+</body>
+</html>
